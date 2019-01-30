1f0c11c6dcf5c6a6e03e6b231dfba20a3cd9c1f5
yaanhyy <yaanhyy@gmail.com>
2019-01-30 18:27:13 +0800

    restruct SendTransaction,need input two args

--- dapp_interface.js
+++ dapp_interface.js
@@ -18,42 +18,42 @@ Tx = require('ethereumjs-tx');
 // let keystore = JSON.parse(keystoreStr)
 // let decryptedAccount = web3.eth.accounts.decrypt(keystore, '123');
 //
-// let rawTransaction = {
+let rawTransaction = {
+
+    "from": "0x5cdb3d471f319a481a375f95ee557ce3acb3588c",
+
+    "to": "0xfd7cdbf6cc424bfa04c556b3863a62b57209f40b",
+
+    "value": web3.utils.toHex(web3.utils.toWei("50", "ether")),
+
+    "gas": 200000,
+
+    "chainId": 50
+
+};
+
+
+async function SendTransaction(rawTransaction, decryptedAccount) {
+    let signTx = await web3.eth.accounts.signTransaction(rawTransaction, decryptedAccount.privateKey)
+    let returnResult = "err"
+
+    try {
+        await web3.eth.sendSignedTransaction(signTx.rawTransaction, function (error, hash) {
+            if (!error) {
+                returnResult = hash
+            } else {
+                returnResult = "101"
+
+
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
+    console.log(returnResult)
+}
 //
-//     "from": "0x5cdb3d471f319a481a375f95ee557ce3acb3588c",
-//
-//     "to": "0xfd7cdbf6cc424bfa04c556b3863a62b57209f40b",
-//
-//     "value": web3.utils.toHex(web3.utils.toWei("50", "ether")),
-//
-//     "gas": 200000,
-//
-//     "chainId": 50
-//
-// };
-// let returnResult
-//
-// async function SendTransaction() {
-//     let signTx = await web3.eth.accounts.signTransaction(rawTransaction, decryptedAccount.privateKey)
-//
-//
-//     try {
-//         await web3.eth.sendSignedTransaction(signTx.rawTransaction, function (error, hash) {
-//             if (!error) {
-//                 returnResult = hash
-//             } else {
-//                 returnResult = "101"
-//
-//
-//             }
-//         })
-//     } catch (error) {
-//         console.log(error)
-//     }
-//     console.log(returnResult)
-// }
-//
-// //SendTransaction()
+//SendTransaction(rawTransaction, decryptedAccount)
 //
 // /**
 //  * erc20 transfer
